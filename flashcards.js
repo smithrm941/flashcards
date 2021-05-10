a981c8edf97fa21e1ccd8bb7f1020ea870c28cd7
smithrm941 <smithrm941@gmail.com>
2021-05-10 00:12:53 -0700

    currently displaying one card of data WIP additional cards

---
+++ flashcards.js
@@ -0,0 +1,1 @@
+import flashcardCollection from './index.js'
--- index.js
+++ index.js
@@ -28,9 +28,60 @@ function myFunction() {
     flashcardCount += 1;
     txt = "You've added " + flashcardCount + " flashcards to your set!";
   }
+  document.getElementById("creationSuccess").innerHTML = txt;
+  flashcardCollection.forEach(card => {
+
+  })
+  document.getElementById("clickForCards").style.display = "block";
+}
+
+// Modal code from W3 schools: https://www.w3schools.com/howto/howto_css_modals.asp
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal, which will display cards
+var cardDisplayButton = document.getElementById("clickForCards");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+cardDisplayButton.onclick = function() {
+  modal.style.display = "block";
+  document.getElementById("cardFront").style.display = "block";
+  document.getElementById("cardFront").innerHTML = flashcardCollection[0].front;
+}
+
+// Get the buttons that flips cards
+var frontButton = document.getElementById("frontButton");
+var backButton = document.getElementById("backButton");
 
 
+// When user clicks the button, show info from back of cards
+backButton.onclick = function() {
+  if (document.getElementById("cardFront").style.display = "block") {
+    document.getElementById("cardFront").style.display = "none";
+    document.getElementById("cardBack").style.display = "block";
+    document.getElementById("cardBack").innerHTML = flashcardCollection[0].back;
+  }
+}
 
-  document.getElementById("demo").innerHTML = txt;
+frontButton.onclick = function() {
+  if (document.getElementById("cardBack").style.display = "block") {
+    document.getElementById("cardBack").style.display = "none";
+    document.getElementById("cardFront").style.display = "block";
+    document.getElementById("cardFront").innerHTML = flashcardCollection[0].front;
+  }
+}
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
